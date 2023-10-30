35eb3ffea6e8faf49a94e5c2057650d1738e22b0
Raj kumar Thavti <rajkumar.thavti@gmail.com>
2023-10-30 15:05:27 +0530

    pograms updated
    pograms updated

---
+++ continue_break/continue.py
@@ -0,0 +1,16 @@
+# Python program to 
+# demonstrate continue 
+# statement 
+
+# loop from 1 to 10 
+for i in range(1, 11): 
+
+	# If i is equals to 6, 
+	# continue to next iteration 
+	# without printing 
+	if i == 6: 
+		continue
+	else: 
+		# otherwise print the value 
+		# of i 
+		print(i, end = " ")
